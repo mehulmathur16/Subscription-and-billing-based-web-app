9cbf8416bd65d5561444ed42557a347d9a5ce040
mehulmathur16 <mathur.mehul01@gmail.com>
2022-12-11 18:47:55 +0530

    added PORT env

--- backend/index.js
+++ backend/index.js
@@ -168,6 +168,8 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.listen(5000, function () {
-    console.log('Example app listening on port 5000!');
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, function () {
+    console.log(`Server started on port ${PORT}`);
 });
